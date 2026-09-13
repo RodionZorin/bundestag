d54c21564f55680de18c31ba9944758413d3c1ea
Rodion Zorin <zorinra1984@gmail.com>
2026-09-13 20:50:57 +0300

    Introduce a function to collect paragraph spans

--- model/call.py
+++ model/call.py
@@ -22,32 +22,38 @@ with open (TEMPLATE, "r", encoding="utf-8") as f:
 with open(PATH, "r", encoding="utf-8") as f:
     data = json.load(f)
 
-gold_labels = {}
-predictions = {}
-paragraphs = []
-spans = {}
-failed_paragpraphs = 0
+#add essential collections
+gold_labels = {} 
+predictions = {} 
+paragraphs = [] 
+spans = {}  
+failed_paragpraphs = 0 
 
-#take each paragraph and form a prompt
-for paragraph in data:
-    paragraph_id = paragraph["id"]
-    #if paragraph_id == 124:
-     #   break
-    paragraphs.append(paragraph_id)
-    annotation = paragraph["annotations"]
-    paragraph_text = paragraph["data"]["text"]
-
-    #collect spans of the text in a list
-    spans_paragraph = []
-    gold_labels_paragraph = []
+def get_paragraph_spans_and_labels(annotation):
+    """
+    Collect spans and their gold labels
+    """
+    paragraph_spans = []
+    paragraph_gold_labels = []
     for span in annotation[0]["result"]:
         from_name = span.get('from_name')
         if from_name in ["premise_type", "claim_type", "other_type"]:
             span_text = span.get("value").get("text")
-            spans_paragraph.append(span_text)
+            paragraph_spans.append(span_text)
             span_gold = span.get("value").get("choices")[0]
-            gold_labels_paragraph.append(span_gold)
-    spans[paragraph_id] = spans_paragraph
+            paragraph_gold_labels.append(span_gold)
+    return paragraph_spans, paragraph_gold_labels
+
+#take each paragraph
+for paragraph in data:
+    paragraph_id = paragraph["id"]
+    paragraphs.append(paragraph_id)
+    annotation = paragraph["annotations"]
+    paragraph_text = paragraph["data"]["text"]
+
+    #collect spans of the paragraph and gold labels of the each span
+    paragraph_spans, paragraph_gold_labels = get_paragraph_spans_and_labels(annotation)
+    spans[paragraph_id] = paragraph_spans
 
     #create context to fill in the gaps of the template
     context = {}
@@ -71,12 +77,12 @@ for paragraph in data:
     if type(model_response) == str:
         model_response = json.loads(model_response)
 
-    if len(model_response) != len(spans_paragraph):
+    if len(model_response) != len(paragraph_spans):
         print("Oops! The model failed to generate correct number of predictions")
         failed_paragpraphs += 1
         continue
 
-    if type(model_response) != type(spans_paragraph):
+    if type(model_response) != type(paragraph_spans):
         print("Oops! The model failed to produce a list of predictions")
         failed_paragpraphs += 1
         continue
@@ -84,7 +90,7 @@ for paragraph in data:
     print("The model generated the correct number of predictions in the correct format")
 
     predictions[paragraph_id] = model_response
-    gold_labels[paragraph_id] = gold_labels_paragraph
+    
 
 #calculate accuracy
 attempts = 0
